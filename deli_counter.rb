cb9c822ba8a490501732898a9577bcc8775a5c8e
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-03-08 23:09:07 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,7 +18,7 @@ def take_a_number(katz_deli, name)
     katz_deli << "#{name}"
     puts "Welcome, #{katz_deli[0]}. You are number 1 in line."
   else
-    katz_deli << "#{person}"
+    katz_deli << "#{name}"
   puts "Welcome, #{katz_deli[-1]}. You are number 2 in line."
 end
 end
